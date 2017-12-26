487f1cd04d59a4d61bc3c31a277a7b1332355dff
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-26 18:55:07 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 var express = require('express')
 var app = express()
 
+// Load Controllers
+const SiteController = "./controllers/SiteController"
 
+// Routes
+app.get("/", SiteController.Index)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
