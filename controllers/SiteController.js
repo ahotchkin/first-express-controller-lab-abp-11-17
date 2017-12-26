989a31d91a4f0e90f5ed4f02daff0cf93c6b702c
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-26 18:53:14 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -7,4 +7,18 @@ SiteController.Index = function(req, resp){
   }
 }
 
+SiteController.About = function(req, resp){
+  const aboutText = "About Us"
+  resp.render{
+    aboutText: aboutText
+  }
+}
+
+SiteController.Contact = function(req, resp){
+  const contactText = "Contact Us"
+  resp.render{
+    contactText: contactText
+  }
+}
+
 module.exports = SiteController
