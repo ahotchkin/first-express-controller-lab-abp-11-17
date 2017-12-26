e0fdd8d7563dc82afdaeb9d5d650eec2a31556f8
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-26 18:52:03 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,3 +1,10 @@
 const SiteController = {}
 
+SiteController.Index = function(req, resp){
+  const welcomeText = "This is a home page"
+  resp.render{
+    welcomeText: welcomeText
+  }
+}
+
 module.exports = SiteController
